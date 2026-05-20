40150c349aeec81dd246d7a502471af0278e51e5
Joe <jaltemus87@icloud.com>
2026-05-20 13:57:01 -0400

    Improve Day 1 with personal comments
    Added band name generator functionality with user input.

--- Day-01-05/day-01-band-name-generator.py
+++ Day-01-05/day-01-band-name-generator.py
@@ -1,1 +1,22 @@
-Move Day 1 file into proper folder
+# Day 1 - Band Name Generator - My Version
+
+print("Welcome to the Band Name Generator! 🎸")
+
+# Getting input from the user
+city = input("Which city did you grow up in?\n")
+pet = input("What is the name of a pet you had or like?\n")
+
+# Combining the two answers into one variable
+band_name = city + " " + pet
+
+print("\nYour band name could be: " + band_name)
+
+
+# === My Personal Notes ===
+# When I finished this project I was excited because I was able to understand 
+# both the input() and print() functions. It made me start questioning 
+# what other uses are possible with Python.
+#
+# What I changed from the course version:
+# I combined both the city and pet variables into a single variable 
+# for my solution. It felt cleaner to me.
